03667cf7c667f2e31a51ac1f67ced7b17dfdadbb
cleyou <77749361+cleyou@users.noreply.github.com>
2025-01-06 14:30:29 +0100

    Create DONTREADME.c
    partage de code vers VM

---
+++ DONTREAD/DONTREADME.c
@@ -0,0 +1,17 @@
+// partage de code vers VM
+#include <stdlib.h>
+#include <stdio.h>
+// Structs and functions declarations
+XXX ICI XXX
+// Execution
+int main(void)
+{
+t_point point;
+set_point(&point);
+return (0);
+}
+void set_point(t_point *point)
+{
+point->x = 42;
+point->y = 21;
+}
